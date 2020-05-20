2b978dbd03c277a28ceebe91235c800308ca69ac
Arshdeep Singh <arshdeeps@ancubate.com>
2020-05-20 02:08:24 -0500

    final lecture

--- server/index.js
+++ server/index.js
@@ -2,41 +2,41 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-
-const keys = require("./config/keys");
-const authRoutes = require("./routes/authRoutes");
-const billingRoutes = require("./routes/billingRoutes");
-
-require("./models/User");
-const passportConfig = require("./services/passport");
 const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-authRoutes(app);
-billingRoutes(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
-mongoose.connect(keys.mongoURI);
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
 
-if (process.env.NODE_ENV === 'production') {
-  // if express finds production
-  app.use(express.static('client/build'));
-
-  // if the route don't exist
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
